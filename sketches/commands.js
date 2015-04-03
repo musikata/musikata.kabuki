8d2ece29357dcf6adc9527933e77deb1c685303a
adorsk <adorsk@gmail.com>
2015-04-03 16:50:00 -0400

    more thoughts on command queue

--- sketches/commands.js
+++ sketches/commands.js
@@ -35,3 +35,72 @@ cmdGroup2:
 // Basically the goal of what we're trying to do is make it easy to write scripts that are readable.
 // And then to make it possible to implement those scripts without having to do all nitty gritty infrastructure to make it work.
 // Just basic scripting.
+
+// Here's one idea:
+// Process a queue iteratively and recursively.
+// Use promises to delay processing the next queue item
+// if desired. (Each commmand returns a promise or value).
+// Find response commands before processing and wire them up
+// ahead of time.
+function processScriptQueue(script, recursionLevel:0) {
+
+  if(! script.promise) {
+    if (script.isSynchronousCmd) {
+      script.dfd = new Deferred();
+      script.promise = dfd.promise();
+    } else{
+      script.promise = 1;
+    }
+  }
+
+  if (recursionLevel = 0) {
+    // First dig out responses and wire them, so that
+    // they're ready to go when we start.
+    var responses = findResponses(script);
+    wireResponses(responses);
+  }
+
+  // If no more commands, script is done running.
+  if ! script.commands:
+    script.dfd.resolve();
+
+  // Then start processing.
+  // Each iteration of this function processes a single command.
+  command = script.commands.pop();
+  if command isa script {
+    result = processScriptQueue(command.data.script, recursionLevel+1)
+  } else {
+    result = doCommand(command);
+  }
+
+  when(result).then(function(){
+    processScriptQueue(script, recursionLevel);
+  });
+
+  return script.promise;
+};
+
+function findResponses(script) {
+  var responses = [];
+  for command in script:
+    if command isa script:
+      responses.extend(findResponses(command));
+    elif command is a response:
+      responses.push(command);
+  return responses;
+  // Note: this is a simple implementation.
+  // It does not handle the case when there are duplicate responses
+  // in a single script block.
+  // For example, something like:
+  // ---
+  // scriptId: 'myScript'
+  // startMusic 'music'
+  // when 'music' hits chorus say 'chorus' once
+  // when 'music' hits chorus say 'chorus' once
+  // ---
+  // For the above script, we have two responses (when 'music' ...)
+  // that respond to the same event.
+  // We could solve this by grouping responses queues, where each group is keyed
+  // by the id of the script block.
+  // Then we would process responses in turn whenever the signal is raised.
+}
