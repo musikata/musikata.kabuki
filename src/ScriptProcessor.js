d9ac4e88c35c883c91f83c253954d7b061c33c9d
adorsk <adorsk@gmail.com>
2015-04-06 17:24:03 -0400

    very basic start of script processor

--- src/ScriptProcessor.js
+++ src/ScriptProcessor.js
@@ -1,4 +1,13 @@
 class ScriptProcessor {
-};
+    constructor(options) {
+        this.commandHandler = options.commandHandler;
+    }
+
+    processScript(script) {
+        script.commands.forEach((cmd) => {
+            this.commandHandler.handle(cmd);
+        });
+    }
+}
 
 module.exports = ScriptProcessor;
--- src/ScriptProcessor.spec.js
+++ src/ScriptProcessor.spec.js
@@ -1,7 +1,39 @@
 var ScriptProcessor = require('./ScriptProcessor');
 
 fdescribe('ScriptProcessor', function() {
+
+    class MockCommandHandler {
+        handle(cmd) {
+        }
+    }
+
+
+    function generateScriptProcessor() {
+        return new ScriptProcessor({
+            commandHandler: new MockCommandHandler()
+        });
+    }
+
     it('should be defined', function() {
         expect(ScriptProcessor).toBeDefined();
+    });
+
+    it('should process sequential commands', function(){
+        var scriptProcessor = generateScriptProcessor();
+        var handler = scriptProcessor.commandHandler;
+        spyOn(handler, 'handle');
+
+        var script = {
+            commands: [
+                {cmd: 'a'},
+                {cmd: 'b'}
+            ]
+        }
+
+        scriptProcessor.processScript(script);
+
+        expect(handler.handle).toHaveBeenCalledWith({cmd: 'a'});
+        expect(handler.handle).toHaveBeenCalledWith({cmd: 'b'});
+
     });
 });
