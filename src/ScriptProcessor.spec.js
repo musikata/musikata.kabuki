65ae593527fc47cd9f2c34dc085bb02224b24ab0
adorsk <adorsk@gmail.com>
2015-04-14 18:15:33 -0400

    started test for blocking commands

--- src/ScriptProcessor.spec.js
+++ src/ScriptProcessor.spec.js
@@ -1,9 +1,27 @@
+var $ = require('jquery');
+
 var ScriptProcessor = require('./ScriptProcessor');
+
 
 fdescribe('ScriptProcessor', function() {
 
     class MockCommandHandler {
+        constructor() {
+            this.promises = {};
+        }
+
         handle(cmd) {
+            if (cmd.data && cmd.data.blocking) {
+                var dfd = new $.Deferred();
+
+                setTimeout(function() {
+                    dfd.resolve();
+                }, 100);
+
+                var promise = this.promises[cmd.id] = dfd.promise();
+
+                return promise;
+            }
         }
     }
 
@@ -36,4 +54,39 @@ fdescribe('ScriptProcessor', function() {
         expect(handler.handle).toHaveBeenCalledWith({cmd: 'b'});
 
     });
+
+    fit('should wait for blocking sequential commands', function(done){
+        var scriptProcessor = generateScriptProcessor();
+        var handler = scriptProcessor.commandHandler;
+        spyOn(handler, 'handle').and.callThrough();
+
+        var script = {
+            commands: [
+                {id: 'a', data: {blocking: true}},
+                {id: 'b'}
+            ]
+        }
+
+        scriptProcessor.processScript(script);
+
+        expect(handler.handle).not.toHaveBeenCalledWith(
+            jasmine.objectContaining({id: 'a'}));
+
+        expect(handler.handle).not.toHaveBeenCalledWith(
+            jasmine.objectContaining({id: 'b'}));
+
+        handler.promises['a'].then(function() {
+            expect(handler.handle).toHaveBeenCalledWith(
+                jasmine.objectContaining({id: 'a'}));
+
+            expect(handler.handle).not.toHaveBeenCalledWith(
+                jasmine.objectContaining({id: 'b'}));
+        }).done(function() {
+            expect(handler.handle).toHaveBeenCalledWith(
+                jasmine.objectContaining({id: 'b'}));
+                done();
+        });
+
+    });
+
 });
