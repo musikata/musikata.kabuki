b7ef85586f8c90eeff270c488fcc93de718bb7f1
adorsk <adorsk@gmail.com>
2015-04-16 11:50:49 -0400

    add basic command handler stub

---
+++ src/CommandHandler.js
@@ -0,0 +1,15 @@
+/**
+ * CommandHandler: for handling script commands.
+ * @TODO: Flesh this out!
+ **/
+
+class CommandHandler {
+    constructor() {
+    }
+
+    handle(cmd) {
+        console.log('handle', cmd);
+    }
+}
+
+module.exports = CommandHandler;
