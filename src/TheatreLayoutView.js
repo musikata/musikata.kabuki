12e61f217d6f055c3449348989c81e8d2bf7a99a
adorsk <adorsk@gmail.com>
2015-04-24 18:07:58 -0400

    add stage view to theatrelayout

--- src/TheatreLayoutView.js
+++ src/TheatreLayoutView.js
@@ -2,6 +2,7 @@ var Marionette = require('kabuki/src/marionette-shim');
 var _ = require('underscore');
 
 var ControlsView = require('./ControlsView');
+var StageView = require('./StageView');
 
 
 var TheatreLayoutView = Marionette.LayoutView.extend({
@@ -14,13 +15,27 @@ var TheatreLayoutView = Marionette.LayoutView.extend({
 
     initialize: function(opts) {
         this.channel = opts.channel;
+
+        // Proxy region commands to stage.
         this.channel.reply('region:get', (regionOpts) => {
-            return this.getRegion('stage');
+            return this.stageView.getRegion(regionOpts.id);
+        });
+
+        this.channel.reply('region:add', (regionOpts) => {
+            return this.stageView.addRegion(regionOpts);
+        });
+
+        this.channel.reply('region:remove', (regionOpts) => {
+            return this.stageView.removeRegion(regionOpts);
         });
     },
 
     onRender: function() {
+        this.showChildView('stage', new StageView({channel: this.channel}));
+        this.stageView = this.getRegion('stage').currentView;
+
         this.showChildView('controls', new ControlsView());
+
     }
 });
 
