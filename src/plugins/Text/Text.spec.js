666ca64e8abe2a040af7e76204ee09a169a8ffe6
adorsk <adorsk@gmail.com>
2015-04-30 10:24:39 -0400

    update text test for animated text

--- src/plugins/Text/Text.spec.js
+++ src/plugins/Text/Text.spec.js
@@ -23,12 +23,14 @@ describe('TextWidget', function() {
     expect(Text.widgets.TextWidget).toBeDefined();
   });
 
-  it('should show text', function() {
+  it('should show text', function(done) {
     var tw = new Text.widgets.TextWidget({channel: channel});
     $container.append(tw.$el);
     tw.render();
     var testText = 'The canteloupe is the queen of fruits.';
-    tw.showText(testText);
-    expect($container.html()).toContain(testText);
+    tw.showText(testText).then(function() {;
+        expect($container.text()).toContain(testText);
+        done();
+    })
   });
 });
